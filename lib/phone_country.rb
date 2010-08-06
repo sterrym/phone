88ee8135a300692fce7772824009d9c326a35584
Tim Glen <tim@tag.ca>
2010-08-06 17:01:18 -0400

    Added an interface to access the CSV/YAML files from external scripts

--- lib/phone_country.rb
+++ lib/phone_country.rb
@@ -1,10 +1,13 @@
 class PhoneCountry < Struct.new(:name, :country_code, :char_2_code, :area_code)
   cattr_accessor :all
   
+  YML_FILE = File.join(File.dirname(__FILE__), '..', 'data', 'phone_countries.yml')
+  CSV_FILE = File.join(File.dirname(__FILE__), '..', 'data', 'phone_countries.csv')
+
   def self.load
     return @@all if @@all.present?
     
-    data_file = File.join(File.dirname(__FILE__), '..', 'data', 'phone_countries.yml')
+    data_file = YML_FILE
     
     @@all = {}
     YAML.load(File.read(data_file)).each_pair do |key, c|
